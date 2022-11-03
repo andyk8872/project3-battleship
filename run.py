b436eafbe4a9740f44f6f1a91ac9e5af90df976c
andyk8872 <andrewkennedy35@yahoo.ie>
2022-11-03 16:52:29 +0000

    feat: add a function (clear()) to clear the screen

--- run.py
+++ run.py
@@ -8,3 +8,13 @@ COMP_SEEN_GRID = [['.']*5 for x in range(5)]
 PLAYER_SEEN_GRID = [['.']*5 for x in range(5)]
 NO_OF_SHIPS = 0
 COMP_SCORE = 0
+
+
+def clear():
+    """
+    A function to clear the screen
+    """
+    os.system('clear')
+
+
+clear()
