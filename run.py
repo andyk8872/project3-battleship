f8635d0a2a01aaa45a32b234ad3f0316a96211a2
andyk8872 <andrewkennedy35@yahoo.ie>
2022-11-03 16:45:14 +0000

    feat: add global variables for computer score and number of ships in game

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ import pyfiglet
 COMP_UNSEEN_GRID = [['.']*5 for x in range(5)]
 COMP_SEEN_GRID = [['.']*5 for x in range(5)]
 PLAYER_SEEN_GRID = [['.']*5 for x in range(5)]
-
+NO_OF_SHIPS = 0
+COMP_SCORE = 0
